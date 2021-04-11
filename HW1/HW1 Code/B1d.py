69c9198bf46a30f681d640d85fc312833e44a690
alto <victor1301166040@gmail.com>
2021-04-10 18:39:02 -0700

    d

--- HW1/HW1 Code/B1d.py
+++ HW1/HW1 Code/B1d.py
@@ -16,7 +16,11 @@ import matplotlib.pyplot as plt
 scatter = plt.scatter
 plot = plt.plot
 show = plt.show
-
+xticks = plt.xticks
+title = plt.title
+legend = plt.legend
+save = plt.savefig
+ylabel = plt.ylabel
 
 def f(x):
     return 4*sin(x*PI)*cos(6*PI*x)
@@ -27,39 +31,91 @@ def Epsilon(length):
 
 
 def GenerateData(n=256):
-    Xgrid = arange(0, 1, 1/256)
+    Xgrid = array([(II + 1)/n for II in range(n)])
     return Xgrid, f(Xgrid) + Epsilon(n)
 
 
-def fHat(data, m, n=256):
+def yHat(data, m, n=256):
+    """
+        Given the data, fit it with average data points on each of the partition.
+    Args:
+        data:
+        m:
+        n:
+    Returns:
+        Predicted value
+    """
     y = zeros(n)
-    for j in range(1, int(n/m) + 1):
-        UpperBound = min(j*m, n)
-        y[(j - 1)*m: UpperBound] = mean(data[(y - 1)*m: UpperBound])
+    for JJ in range(int(n/m)):
+        UpperBound = min((JJ + 1)*m, n)
+        LowerBound = JJ*m
+        y[LowerBound: UpperBound] = mean(data[LowerBound: UpperBound])
     return y
 
 
 def fBar(xgrid, m, n=256):
-    return fHat(f(xgrid), m, n)
+    return yHat(f(xgrid), m, n)
 
 
-def AvgBiasSqaured(data):
-    pass
+def AvgBiasSqaured(m, n=256):
+    """
+        The expected vaue of the biases error squared. Because it's expected value,
+        this will use the underlying generative model instead of using data to get
+        the error from the biases squared.
+
+    Args:
+        data:
+
+    Returns:
+
+    """
+    Xgrid = array([(II + 1)/n for II in range(n)])
+    FBar = fBar(Xgrid, m, n)
+    F = f(Xgrid)
+    return mean((FBar - F)**2)
 
 
-def AvgVariance(data):
-    pass
+def AvgVariance(m):
+    return 1/m
 
 
-def AvgEmpiricalErr(data):
-    pass
+def AvgEmpiricalErr(yhat, n=256):
+    Xgrid = array([(II + 1)/n for II in range(n)])
+    return mean((f(Xgrid) - yhat)**2)
 
 
 def main():
-    Xgrid, Data = GenerateData()
-    for m in [2**II for II in range(6)]:
-        ...
-    pass
+    def FitDemo():
+        Xs, Ys = GenerateData()
+        scatter(Xs, Ys)
+        Yhat = yHat(Ys, 8)
+        YBar = fBar(Xs, 8)
+        plot(Xs, Yhat, c="red")
+        plot(Xs, YBar, c="green")
+        show()
+    FitDemo()
+    def PlotErrors():
+        Error1 = [] # Empirical error from 256 random samples.
+        Error2 = [] # Expected Bias Squared
+        Error3 = [] # Expcted Variance Square
+        Error4 = [] # Expected Errors
+        _, SampledData = GenerateData()
+        for m in [2**II for II in range(6)]:
+            Error1.append(AvgEmpiricalErr(yHat(SampledData, m)))
+            Error2.append(AvgBiasSqaured(m))
+            Error3.append(AvgVariance(m))
+            Error4.append(Error2[-1] + Error3[-1])
+        plot(Error1)
+        plot(Error2)
+        plot(Error3)
+        plot(Error4)
+        xticks(range(6), [f"m=$2^{II}$" for II in range(6)])
+        ylabel("Error")
+        legend(["Empirical Error", "E[Bias^2]", "E[Var]", "E[Bias^2 + Var]"])
+        title("B.1(d): Variance Decomposition")
+        save("B.1(d)plot.png")
+
+    PlotErrors()
 
 
 if __name__ == "__main__":
