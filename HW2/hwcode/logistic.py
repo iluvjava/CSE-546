b700a0989518777b3f7e5f57e99db358ea7d4f7d
alto laptop wsl unbuntu 20.04 <victor1301166040@gmail.com>
2021-05-05 10:29:15 -0700

    a lot of new changes

---
+++ HW2/hwcode/logistic.py
@@ -0,0 +1,16 @@
+### This is for CSE 546 2021 Spring
+### Name: Honda Li
+### Don't copy my code cause my code has my style in it.
+
+class BinaryLogisticRegression:
+    
+    def __init__(this, regularizer_lambda=1e-1):
+        this._Lambda = regularizer_lambda
+
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
