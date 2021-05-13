6425a79d1c43f085cae758da23cbbba202f5510f
alto laptop wsl unbuntu 20.04 <victor1301166040@gmail.com>
2021-05-13 11:53:47 -0700

    stuff

--- HW3/hw3code/kernel_ridge_regression.py
+++ HW3/hw3code/kernel_ridge_regression.py
@@ -3,13 +3,19 @@
 ### Author: Hongda Li
 
 import numpy as np
-import scipy
+from scipy import linalg
+import matplotlib.pyplot as plt
 
-invh = scipy.linalg.invh
+linspace = np.linspace
+randn = np.random.randn
+pinvh = linalg.pinvh
+inv = linalg.inv
 eye = np.eye
 mean = np.mean
+std = np.std
 
 class KernelRidge:
+
     def __init__(this, regularizer_lambda, kernelfunc: callable):
         """
 
@@ -22,9 +28,8 @@ class KernelRidge:
         this.KernelMatrix = None
         this.X = None
         this.Kernel = kernelfunc
-
         this.Alpha = None
-        this.OffSet = None
+        this.Bias = None
 
     def fit(this, X, y):
         """
@@ -33,7 +38,7 @@ class KernelRidge:
         :param y:
         :return:
         """
-        assert type(X) == np.array and type(y) == np.array, "X, y, must be numpy array"
+        assert type(X) is np.ndarray and type(y) is np.ndarray, "X, y, must be numpy array"
         assert X.ndim == 2 and y.ndim <= 2
         Warn = "X, y dimension problem"
         if y.ndim == 2:
@@ -41,42 +46,47 @@ class KernelRidge:
             assert y.shape[1] == 1, Warn
         else:
             assert y.shape[0] == X.shape[0], Warn
-            y = y[-1, np.newaxis]
-        this.X = X
+            y = y[:, np.newaxis]
+        # Standardized.
+        this.X = (X - mean(X, axis=0, keepdims=True))/std(X, axis=0, keepdims=True)
         n, d = X.shape
         Lambda = this.Lambda
-        K = this.Kernel(X)
-        assert K.ndim == 2 and K.shape[0] == K.shape[2] and K.shape[0] == n, \
+        K = this.Kernel(this.X)
+        assert K.ndim == 2 and K.shape[0] == K.shape[1] and K.shape[0] == n, \
             "your kernel function implemention is wrong, kernel matrix is having the wrong shape"
         assert np.all(np.abs(K-K.T) < 1e-9), "kernel matrix is not symmetric."
         this.KernelMatrix = K
-        ## Standardize
-        this.OffSet = mean(X, axis=1, keepdims=X.shape[1] != 1)
-        this.Alpha = invh(K + Lambda*eye(n))@y
+        # get the bias
+        this.Bias = mean(y.reshape(-1))
+        # get the alpha.
+        this.Alpha = pinvh(K + Lambda*eye(n))@(y - this.Bias)
 
 
     def predict(this, Xtest):
         assert this.X is not None, "Can't predict when not trained yet. "
-        Xtest = Xtest - this.OffSet
         Xtrain = this.X
-        return Xtest@Xtrain.T@this.Alpha + this.OffSet
+        XtestStd = (Xtest - mean(Xtest, axis=0, keepdims=True))/std(Xtest, axis=0, keepdims=True)
+        return XtestStd@Xtrain.T@this.Alpha + this.Bias
 
 
 
 def main():
-    import matplotlib.pyplot as plt
-    linspace = np.linspace
-    randn = np.random.randn()
     def SimpleTest():
         N = 100
         w, b = 1, 1
-        x = linspace(0, 1, N)
-        eps = randn(N)*0.1
+        x = linspace(0, 1000, N)
+        eps = randn(N)*100
         y = w*x + b + eps
         X = x[:, np.newaxis]
-
-        pass
-    pass
+        def KernelFunc(X):
+            return X@X.T
+        Model = KernelRidge(regularizer_lambda=0.01, kernelfunc=KernelFunc)
+        Model.fit(X, y)
+        Yhat = Model.predict(X)
+        plt.plot(x, y)
+        plt.plot(x, Yhat)
+        plt.show()
+    SimpleTest()
 
 
 if __name__ == "__main__":
