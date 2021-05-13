63b45e33f9f5d10c417ab8687d790a26fe7e2d92
alto laptop wsl unbuntu 20.04 <victor1301166040@gmail.com>
2021-05-13 14:42:27 -0700

    changes HW3

---
+++ HW3/hw3code/A3.py
@@ -0,0 +1,37 @@
+### This is the script that produce plots and data for A3
+### This is for CSE 546 SPRING 2021, HW3.
+### Author: Hongda Alto Li
+### Requries: kernel_ridge_regression.py
+
+from kernel_ridge_regression import KernelRidge
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics.pairwise import rbf_kernel, polynomial_kernel
+
+
+def KBFKernel(X, delta):
+    """
+        X is the row data matrix.
+    :param x:
+    :return:
+    """
+
+    pass
+
+
+def PolyKernel(X, d):
+    """
+        X is the row data matrix.
+    :param x:
+    :return:
+    """
+
+    pass
+
+
+def main():
+
+    pass
+
+if __name__ == "__main__":
+    main()
