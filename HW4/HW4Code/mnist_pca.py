ca67e80973eb7133d7eef377dbc47db6558d490e
alto laptop wsl unbuntu 20.04 <victor1301166040@gmail.com>
2021-06-05 16:23:01 -0700

    ML Real worl

--- HW4/HW4Code/mnist_pca.py
+++ HW4/HW4Code/mnist_pca.py
@@ -1,17 +1,14 @@
 # This is for HW4 A3
 # Course: CSE 546, SPRING 2021
 # Name: Hongda Li
-# My code has my tyle in it please donm't copy.
+# My code has my tyle in it please don't copy.
 
 
+ 
 
 
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    import os
+    print(os.getcwd())
+    print(os.curdir)
 
