4a6582dedf635062b4b6becc131e9ffbb76cded1
alto laptop wsl unbuntu 20.04 <victor1301166040@gmail.com>
2021-06-07 22:59:51 -0700

    The end

--- HW4/HW4Code/matrix_completion.py
+++ HW4/HW4Code/matrix_completion.py
@@ -7,7 +7,8 @@ from scipy.sparse.linalg import svds
 import matplotlib.pyplot as plt
 from math import isnan
 import csv
-
+zeros = np.zeros
+pinv = np.linalg.pinv
 DATA_PATH = "./data/ml-100k/u.data"
 
 
@@ -81,6 +82,59 @@ def Epsilon(approx, train=True):
         DiffSum += (Rating - approx[II, JJ]) ** 2
     return DiffSum / Idx
 
+
+class AlternatingMinimization:
+
+    def __init__(this,
+                 dataMatrix:np.ndarray,
+                 d:int,
+                 sigma,
+                 regularizer,
+                 tol=1e-2):
+        assert dataMatrix.ndim == 2
+        m, n = dataMatrix.shape
+        this.m, this.n = m, n
+        this.R = dataMatrix
+        this.Sigma = sigma
+        this.Lambda = regularizer
+        this.Tol = tol
+        this.Rank = d
+        this.V = sigma*np.random.randn(d, n)
+        this.U = sigma*np.random.randn(d, m)
+        this.I = np.eye(d)
+        this.M = np.array(dataMatrix != 0, dtype=np.float)
+
+    def UOpt(this):
+        L = this.Lambda
+        I = this.I
+        R = this.R
+        V = this.V
+        U = this.U
+        M = this.M
+        for K in range(this.m):
+            U[:, K:K+1] = pinv(V@(M[K:K + 1,:].T*V.T) + L*I)@(V@R[K:K+1, :].T)
+
+    def VOpt(this):
+        L = this.Lambda
+        I = this.I
+        R = this.R
+        V = this.V
+        U = this.U
+        M = this.M
+        for K in range(this.n):
+            V[:, K:K+1] = pinv(U@(M[:, K:K+1]*U.T) + L*I)@(U@R[:, K:K+1])
+
+    def TrainLoss(this):
+        return this.Loss()
+
+    def TestLoss(this):
+        return this.Loss(False)
+
+    def Loss(this, train=True):
+        Approx = this.U.T@this.V
+        return Epsilon(Approx, train=train)
+
+
 def PartA():
     DiffSum = 0
     for Idx, (II, _, Rating) in enumerate(
@@ -88,6 +142,7 @@ def PartA():
         ):
         DiffSum += (Rating - TRAIN_AVG[II, 0])**2
     return DiffSum/Idx
+
 
 def PartB(ranks=[1, 2, 5, 10, 20, 50]):
     Ranks = sorted(ranks + [0])
@@ -107,16 +162,33 @@ def PartB(ranks=[1, 2, 5, 10, 20, 50]):
         MSETest.append(Epsilon(Approximation, False))
     return ranks, MSETrain, MSETest
 
+def MatrixComplete(d, sigma, regularizer):
+    Instance = AlternatingMinimization(
+        TRAIN_SPR.asfptype().toarray(),
+        d=d,
+        sigma=sigma,
+        regularizer=regularizer
+    )
+    for II in range(100):
+        Loss = Instance.Loss()
+        Instance.UOpt()
+        Instance.VOpt()
+        print(Loss)
+        if Loss - Instance.Loss() < 1e-2:
+            TestLoss = Instance.TestLoss()
+            break
+    return Loss, TestLoss
+
 
 def main():
     FolderPath = "./B1"
     mkdir(FolderPath)
     def ParA():
         PartAError = PartA()
-        with open(f"{FolderPath}/", "w+") as f:
+        with open(f"{FolderPath}/part-a.txt", "w+") as f:
             f.write(f"For part (a), the error on test set is: {PartAError}")
-        print(f"Fora part (b):")
-    ParA()
+        print(f"ParA Done")
+    # ParA()
     def ParB():
         Ranks, TrainErr, TestErr = PartB()
         print(f"Train Errors {TrainErr, TestErr}")
@@ -126,7 +198,24 @@ def main():
         plt.title("Ranks and Reconstruction (Filled with Zeroes)")
         plt.savefig(f"{FolderPath}/{Ts()}-b1-b.png")
         plt.show()
-    ParB()
+        plt.clf()
+    # ParB()
+    def ParC():
+        Ranks = [1, 2, 5, 10, 20, 50]
+        TrainLosses, TestLosses = [], []
+        for Rank in Ranks:
+            TrainLoss, TestLoss = MatrixComplete(Rank, 1, Rank/10)
+            TrainLosses.append(TrainLoss)
+            TestLosses.append(TestLoss)
+        plt.plot(TrainLosses, "-o")
+        plt.plot(TestLosses, "-o")
+        plt.legend(["Train MSE", "Test MSE"])
+        plt.title("Matrix Complete, Ignore zeros, Alternating Opt")
+        plt.savefig(f"{FolderPath}/{Ts()}-b1-c.png")
+        plt.show()
+        plt.clf()
+    ParC()
+
 
 
 if __name__ == "__main__":
